f66f14ef734aec72a38ed03a0dac43f889a8148a
Phattharapong Tj <42928626+McTj1@users.noreply.github.com>
2025-02-17 15:19:07 +0700

    [Remove] unused file

--- src/main.js
+++ src/main.js
@@ -1,1 +1,8 @@
-import Nakama from './nakama'
+import App from './App.svelte';
+
+const app = new App({
+	target: document.body,
+	props: {}
+});
+
+export default app;
--- src/nakama.js
+++ src/nakama.js
@@ -5,47 +5,50 @@ class Nakama {
   constructor() {
     this.client
     this.session
-    this.socket // ep4
-    this.matchID // ep4
+    this.socket
+    this.matchID
   }
 
+  async login() {}
+
+  //  UnhandledPromiseRejectionWarning: ReferenceError: XMLHttpRequest is not defined not support when run nodejs. **
   async authenticate() {
-    const useSSL = false
-    this.client = new Client('defaultkey', 'localhost', '7350', useSSL)
-    this.client.console.log(this.client, '++++++++++++++++++++++++++ client')
+    try {
+      this.client = new Client('defaultkey', 'localhost', '7350')
+      this.client.ssl = false
 
-    let deviceId = localStorage.getItem('deviceId')
-    if (!deviceId) {
-      deviceId = uuidv4()
-      localStorage.setItem('deviceId', deviceId)
+      let deviceId = uuidv4()
+      // if (!deviceId) {
+      //   deviceId = uuidv4()
+      //   localStorage.setItem('deviceId', deviceId)
+      // }
+
+      this.session = await this.client.authenticateCustom(deviceId, true)
+      // localStorage.setItem('user_id', this.session.user_id)
+
+      // ep4
+      const trace = false
+      this.socket = this.client.createSocket(this.useSSL, trace)
+      await this.socket.connect(this.session)
+    } catch (error) {
+      console.error('Error authenticating:', error)
     }
-
-    this.session = await this.client.authenticateDevice(deviceId, true)
-
-    console.log(this.session, '+++++++++++++++++++++++++ session device')
-
-    localStorage.setItem('user_id', this.session.user_id)
-
-    const trace = false
-    this.socket = this.client.createSocket(this.useSSL, trace)
-
-    console.log(this.socket, '+++++++++++++++++++++++++ socket create socket')
-
-    await this.socket.connect(this.session)
   }
 
   async findMatch(ai = false) {
-    // ep4
     const rpcid = 'find_match'
     const matches = await this.client.rpc(this.session, rpcid, { ai: ai })
     console.log(matches, '**** findMatch ****')
     this.matchID = matches.payload.matchIds[0]
-    await this.socket.joinMatch(this.matchID)
-    console.log('Matched joined!')
+    try {
+      await this.socket.joinMatch(this.matchID)
+      console.log('Matched joined!')
+    } catch (error) {
+      console.error('Error joining match:', error)
+    }
   }
 
   async makeMove(index) {
-    // ep4
     var data = { position: index, namePlayer: 'TJ' }
     await this.socket.sendMatchState(this.matchID, 4, JSON.stringify(data))
     console.log('Match data sent')
